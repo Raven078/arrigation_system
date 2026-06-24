c9b061e46657de9c2260a0ac86b283add0ceba32
EVGENY VORONOV <evoronov@mail.ru>
2026-06-24 16:37:51 +0300

    Delete main directory

--- main/GUI/ui/ui_cucumber.h
+++
@@ -1,7 +0,0 @@
-#ifndef UI_CUCUMBER_H
-#define UI_CUCUMBER_H
-
-void ui_cucumber_show_window(void);
-void ui_cucumber_update_sensors(float temperature, int moisture, bool sensor1_detected, bool sensor2_detected);
-
-#endif
--- main/main/GUI/wallpaper/wallpaper800400.h
+++
@@ -1,8 +0,0 @@
-#ifndef WALLPAPER800400_H
-#define WALLPAPER800400_H
-
-#include "lvgl.h"
-
-extern const lv_img_dsc_t wallpaper800400;
-
-#endif
